361a2d0158e958416684dbd9c22a2cfdc93f13f5
user <adel_abouf@outlook.com>
2020-12-26 11:57:28 +0200

    Render user profile details

--- src/components/ViewUserProfile.js
+++ src/components/ViewUserProfile.js
@@ -5,9 +5,10 @@ import {
     getUserProfile,
     clearViewUserProfileState
 } from "../actions";
-import {  Spinner } from "react-bootstrap";
+import {  Spinner, Image, Card, Form, Carousel } from "react-bootstrap";
 import actionCable from "actioncable";
 import { ACTION_CABLE_ROUTE } from "../actions/types";
+import _ from "lodash";
 
 class ViewUserProfile extends Component{
 
@@ -24,10 +25,10 @@ class ViewUserProfile extends Component{
         const profile_moderation_channel_subscription = null;
 
         this.state = {
-          history,
-          params,
-          cable,
-          profile_moderation_channel_subscription
+            history,
+            params,
+            cable,
+            profile_moderation_channel_subscription
         };
 
     }
@@ -118,9 +119,214 @@ class ViewUserProfile extends Component{
 
     }
 
+
+    renderBlockedBy(){
+
+        const { blocked_by } = this.props;
+
+        if(!_.isEmpty(blocked_by)){
+
+            return(
+
+                <Form.Group>
+
+                    <Form.Label >
+                        Blocked By
+                    </Form.Label>
+
+
+                    <Form.Control
+                        readOnly
+                        type="text"
+                        value={blocked_by}
+                    />
+
+                </Form.Group>
+
+            );
+
+        }
+
+    }
+
+    renderProfileBio(){
+
+        const { profile_bio } = this.props;
+
+        if(!_.isEmpty(profile_bio)){
+
+            return(
+
+                <Form.Group>
+
+                    <Form.Label >
+                        Profile Bio
+                    </Form.Label>
+
+
+                    <Form.Control
+                        as="textarea"
+                        rows={3}
+                        value={profile_bio}
+                        readOnly
+                    />
+
+                </Form.Group>
+
+
+            );
+
+        }
+
+    }
+
+    renderPosts(posts){
+
+        return _.map(posts, (post, index) => {
+
+            console.log(post);
+
+
+            if(post.media_type === "image"){
+
+                return(
+
+                    <Carousel.Item key={index}>
+
+
+                        <Image
+                            src={post.image_file.url}
+                            thumbnail
+                            className="image-post"
+                        />
+
+                    </Carousel.Item>
+
+
+                );
+
+
+            }else{
+
+                return(
+
+                    <Carousel.Item key={index}>
+
+
+                        <video className="video-post"  width="600" height="600"  controls>
+
+
+                            <source src={post.video_file.url} />
+
+                        </video>
+
+                    </Carousel.Item>
+
+                );
+
+            }
+
+
+
+        });
+
+    }
+
+    renderStoryPosts(){
+
+        const { story_posts } = this.props;
+
+        if(story_posts.length > 0){
+
+            return(
+
+
+                <Form.Group>
+
+                    <Form.Label >
+                        Story Posts
+                    </Form.Label>
+
+
+                    <Card className="user-posts-container">
+
+                        <Card.Body>
+
+                            <Carousel interval={null} >
+
+                                {this.renderPosts(story_posts)}
+
+                            </Carousel>
+
+                        </Card.Body>
+
+
+
+                    </Card>
+
+                </Form.Group>
+
+
+            );
+
+        }
+
+
+    }
+
+    renderProfilePosts(){
+
+        const { profile_posts } = this.props;
+
+        if(profile_posts.length > 0){
+
+            return(
+
+
+                <Form.Group>
+
+                    <Form.Label >
+                        Profile Posts
+                    </Form.Label>
+
+
+                    <Card className="user-posts-container">
+
+                        <Card.Body>
+
+                            <Carousel interval={null} >
+
+                                {this.renderPosts(profile_posts)}
+
+                            </Carousel>
+
+                        </Card.Body>
+
+
+
+                    </Card>
+
+
+                </Form.Group>
+
+
+            );
+
+
+        }
+
+    }
+
     show(){
 
-        const { initializing_page } = this.props;
+        const {
+            initializing_page,
+            profile_picture,
+            username,
+            email,
+            user_type,
+            status
+        } = this.props;
 
         if(initializing_page){
 
@@ -143,6 +349,103 @@ class ViewUserProfile extends Component{
             return(
 
                 <div className="page-container">
+
+                    <div className="header-container">
+
+                        <Image
+                            className="profile-photo"
+                            src={_.isEmpty(profile_picture) ? "https://via.placeholder.com/150x180" : profile_picture}
+                            thumbnail
+                        />
+
+                    </div>
+
+                    <div className="account-container">
+
+
+                        <Card id="view-user-profile-card">
+
+                            <Card.Body>
+
+                                <Form>
+
+                                    <Form.Group>
+
+                                        <Form.Label >
+                                            Username
+                                        </Form.Label>
+
+
+                                        <Form.Control
+                                            readOnly
+                                            type="text"
+                                            value={username}
+                                        />
+
+                                    </Form.Group>
+
+
+                                    <Form.Group>
+
+                                        <Form.Label >
+                                            Email
+                                        </Form.Label>
+
+
+                                        <Form.Control
+                                            readOnly
+                                            type="text"
+                                            value={email}
+                                        />
+
+                                    </Form.Group>
+
+                                    <Form.Group>
+
+                                        <Form.Label >
+                                            Account Type
+                                        </Form.Label>
+
+
+                                        <Form.Control
+                                            readOnly
+                                            type="text"
+                                            value={user_type === "customer_user" ? 'Personal' : 'Business'}
+                                        />
+
+                                    </Form.Group>
+
+
+                                    <Form.Group>
+
+                                        <Form.Label >
+                                            Status
+                                        </Form.Label>
+
+
+                                        <Form.Control
+                                            readOnly
+                                            type="text"
+                                            value={_.startCase(status)}
+                                        />
+
+                                    </Form.Group>
+
+                                    {this.renderBlockedBy()}
+
+                                    {this.renderProfileBio()}
+
+                                    {this.renderStoryPosts()}
+
+                                    {this.renderProfilePosts()}
+
+                                </Form>
+
+                            </Card.Body>
+
+                        </Card>
+
+                    </div>
 
                 </div>
 
